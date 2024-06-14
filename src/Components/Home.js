19ca7097ced4f67059d2873ef41e232f01118cec
osuji chiaka <chiakajunior@yahoo.com>
2024-06-14 23:01:33 +0300

    added some projects to home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -16,11 +16,44 @@ const Home = (props) => {
 
             <div className='featuredProjects'> 
             <FeaturedProjects 
+            header= "PERFTRAKA"
+            stacks="HTML, CSS, TAILWIND, LARAVEL, BLADE"
+            description = "A web application for a security company, used for capturing Locations and managing employees logs "
+            type = 'featured'
+            liveLink="https://perftraka.com/"
+            codeLink=""
+            />
+            </div>
+
+            <div className='featuredProjects'> 
+            <FeaturedProjects 
             header= "Slumtech Foundation"
             stacks="Next.js, MONGODB"
             description = "A website for a non-profit organisation, that specializes in training teenagers in different aspect of technology"
             type = 'featured'
             liveLink="https://www.slumtechfoundation.com/about"
+            codeLink=""
+            />
+            </div>
+
+            <div className='featuredProjects'> 
+            <FeaturedProjects 
+            header= "1035 Technology"
+            stacks="Next.js"
+            description = "A website For a Tech, Telecommunication & Energy Consulting Service Company In Nigeria"
+            type = 'featured'
+            liveLink="https://www.1035gtech.com/"
+            codeLink=""
+            />
+            </div>
+
+            <div className='featuredProjects'> 
+            <FeaturedProjects 
+            header= "A2z101"
+            stacks="Next.js"
+            description = "A website for a Project Management company"
+            type = 'featured'
+            liveLink="https://www.atozoneonone.com/"
             codeLink=""
             />
             </div>
@@ -73,7 +106,7 @@ const Home = (props) => {
             
            
 
-            <div className='othersHeader'>
+            {/* <div className='othersHeader'>
             OTHERS
             </div>
 
@@ -165,7 +198,7 @@ const Home = (props) => {
 
 
             </div>
-            </div>
+            </div> */}
             </div>
         
         </div>
